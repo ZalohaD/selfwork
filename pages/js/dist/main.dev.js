2a4578d464fc426deefbd62bd0b44e7fff990cb9
ZalohaD <74576798+ZalohaD@users.noreply.github.com>
2021-04-23 20:44:06 +0300

    last?

---
+++ pages/js/dist/main.dev.js
@@ -0,0 +1,82 @@
+"use strict";
+
+var showMenu = function showMenu(toggleId, navId) {
+  var toggle = document.getElementById(toggleId),
+      nav = document.getElementById(navId);
+
+  if (toggle && nav) {
+    toggle.addEventListener('click', function () {
+      nav.classList.toggle('show');
+    });
+  }
+};
+
+showMenu('nav-toggle', 'nav-menu');
+var rellax = new Rellax('.parallax');
+/*===GSAP SECTION===*/
+
+/*NavBar*/
+
+gsap.from('.nav__logo', {
+  opacity: 0,
+  duration: 3,
+  delay: 2,
+  y: 30,
+  ease: 'expo.out'
+});
+gsap.from('.nav__toggle', {
+  opacity: 0,
+  duration: 3,
+  delay: 7,
+  y: 30,
+  ease: 'expo.out'
+});
+gsap.from('.nav__item', {
+  opacity: 0,
+  duration: 3,
+  delay: 2,
+  y: 35,
+  ease: 'expo.out',
+  stagger: .2
+});
+/* Text*/
+
+gsap.from('.home__title', {
+  opacity: 0,
+  duration: 3,
+  delay: 1.3,
+  y: 35,
+  ease: 'expo.out'
+});
+gsap.from('.home__subtitle', {
+  opacity: 0,
+  duration: 3,
+  delay: 1.1,
+  y: 35,
+  ease: 'expo.out'
+});
+/*scroll*/
+
+gsap.from('.home__scroll', {
+  opacity: 0,
+  duration: 3,
+  delay: 1.1,
+  y: 35,
+  ease: 'expo.out'
+});
+/* SCROLL REVEAL SECTION*/
+
+var sr = ScrollReveal({
+  duration: 2500,
+  reset: true
+});
+sr.reveal('.section__data', {
+  origin: 'left',
+  distance: '70px',
+  delay: 200
+});
+sr.reveal('.section__img', {
+  origin: 'right',
+  distance: '70px',
+  delay: 200
+});
